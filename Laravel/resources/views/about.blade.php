4bd133d5df978d7c2deabc5610ffd9e0b0fdd71d
sergifercla <101187391+sergifercla@users.noreply.github.com>
2023-03-20 19:07:50 +0100

    Laravel

---
+++ Laravel/resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('About') }} Sergi Fernández Sergi Rojas
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <p>Soy Sergi Fernández Sergi Rojas estudiante de 2DAW</p>
+        </div>
+    </div>
+</x-app-layout>
---
+++ Laravel/resources/views/contact.blade.php
@@ -0,0 +1,26 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Contact') }}Sergi Fernández Sergi Rojas
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <h1>Envía tu mensaje</h1>
+            <form action="">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+
+                <input type="submit"></input>
+            </form>
+        </div>
+
+    </div>
+</x-app-layout>
